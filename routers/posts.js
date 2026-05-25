6ac95ee12fdc59bd11e29480004fea4fd22d7aab
Ivan Caldarella <gamein3x@gmail.com>
2026-05-25 15:57:57 +0200

    Creata base per il routing, importati nuovi file

---
+++ routers/posts.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import { index, show, create, destroy } from '../controllers/pizzas.js';
+
+const router = express.Router();
+
+// INDEX localhost:3000/pizzas/
+router.get('/', index);
+
+// SHOW localhost:3000/pizzas/:id
+router.get('/:id', show);
+
+// CREATE localhost:3000/pizzas
+router.post('/', create)
+
+// DELETE
+router.delete('/:id', destroy)
+
+export default router;
